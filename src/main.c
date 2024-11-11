2d4486c6fc29dc88deb88cdaea68545428abd3a9
donnieparka <alberto.iacovella@gmail.com>
2024-11-11 18:23:01 +0100

    last refactor

--- src/main.c
+++ src/main.c
@@ -11,8 +11,8 @@
 
 void print_usage(char *argv[]) {
   printf("Usage: %s -n -f DbFile\n", argv[0]);
-  printf("-n specify if the file has to be created");
-  printf("-f (required) specify the Db path");
+  printf("-n specify if the file has to be created\n");
+  printf("-f (required) specify the Db path\n");
 }
 int main(int argc, char *argv[]) {
   if (argc < 3) {
@@ -30,7 +30,6 @@ int main(int argc, char *argv[]) {
   int count;
   int dbfd = 0;
   struct dbheader_t *header = NULL;
-  struct employee_t *employee = NULL;
   struct employee_t *employees = NULL;
 
   while ((currentArg = getopt(argc, argv, "nf:alhr")) != -1) {
@@ -57,6 +56,8 @@ int main(int argc, char *argv[]) {
       break;
     case '?':
       printf("unknown argument -%c\n", currentArg);
+        print_usage(argv);
+      return STATUS_ERROR;
       break;
     default:
       return STATUS_ERROR;
@@ -68,6 +69,7 @@ int main(int argc, char *argv[]) {
   if (newfile) {
     dbfd = createDb(filePath);
     if (dbfd == -1) {
+      free(header);
       return STATUS_ERROR;
     }
     header = create_db_header();
@@ -80,6 +82,7 @@ int main(int argc, char *argv[]) {
       return STATUS_ERROR;
     }
     if (validate_db_header(dbfd, &header) == STATUS_ERROR) {
+      free(header);
       printf("file validation failed");
       return STATUS_ERROR;
     }
@@ -88,22 +91,32 @@ int main(int argc, char *argv[]) {
     if (employees == NULL && header->count > 0) {
       printf("failed to read employees");
       perror("read");
+      free(header);
       return STATUS_ERROR;
     }
     if (addOne) {
-      header->count++;
-      employees = realloc_employees(employees, header->count);
+      int new_count = header->count + 1;
+      employees = realloc_employees(employees, new_count);
       if (employees == NULL) {
         printf("realloc failed in realloc_employees\n");
+        free(header);
         return STATUS_ERROR;
       }
-      employees = add_employee(employees, header->count);
+      employees = add_employee(employees, new_count);
+      if (employees == NULL) {
+        printf("failed in add_employee");
+        free(header);
+        return STATUS_ERROR;
+      }
+      header->count = new_count;
     }
-    if (header->count == 0) {
-      printf("non ci sono impiegati da listare o modificare");
-      return STATUS_ERROR;
-    }
+
     if (list) {
+      if (header->count == 0) {
+        printf("non ci sono impiegati da listare o modificare");
+        free(header);
+        return STATUS_ERROR;
+      }
       for (int i = 0; i < header->count; i++) {
         printf("-----------------------\n");
         printf("impiegato %d: %s\n", i, employees[i].name);
@@ -127,7 +140,6 @@ int main(int argc, char *argv[]) {
           for (int i = select; i < header->count - 1; i++) {
             employees[i] = employees[i + 1];
           }
-          is_last = false;
         }
         header->count--;
         if (header->count > 0 && !is_last) {
@@ -139,6 +151,7 @@ int main(int argc, char *argv[]) {
             return STATUS_ERROR;
           } else {
             employees = new_employees;
+            printf("employee removed");
           }
         } else {
           free(employees);
